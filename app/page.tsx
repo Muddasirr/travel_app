bf571c54bd87c1c4edfb1220d37929c7764d5244
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-02 15:44:17 +0500

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import Hero from "@/components/Hero";
+import Guide from "@/components/Guide";
+import GetApp from "@/components/GetApp";
+
 
 
 export default function Home() {
   return (
-  <h1>Hello World</h1>
+  <>
+  <Hero/>
+  <Camp/>
+  <Guide/>
+  <Features/>
+  <GetApp/>
+  </>
   )
 }
