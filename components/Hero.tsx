351992427d7b6b36c7a04131593232b4045e359e
Muddasirr <110295334+Muddasirr@users.noreply.github.com>
2024-01-02 16:31:35 +0500

    commit

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import React from 'react'
+import Button from './button'
 
 const Hero = () => {
   return (
@@ -20,6 +21,31 @@ const Hero = () => {
     <p className='regular-16 mt-6 text-gray-30 xl:max-w-[520px]'>
     We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
     </p>
+    <div className='my-11 flex flex-wrap gap-5'>
+        <div className='flex items-center gap-2'>   
+{Array(5).fill(1).map((_,i)=>(
+<Image src="/star.svg" key={i}
+alt='star' width={24} height={24}/>
+
+
+
+))}
+    </div>
+<p className="bold-16 lg:bold-20 text-blue-70">198k
+<span className="regular-16 lg:regular-20 ml-1">
+    Excellent Reviews</span>
+
+</p>
+
+</div>
+<div className="flex w-full gap-3 sm:flex-row">
+    <Button type="button" title="Download App"
+    variant="btn_green"/>
+    <Button type="button" title="How we work?"
+    icon ="/play.svg"
+    variant="btn_white_text"/>
+
+</div>
 </div>
 
 
